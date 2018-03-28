10c1f2e8dd44f8b22b9515defa9de649c202b976
blurblah <blurblah@blurblah.net>
2018-03-28 16:31:01 +0900

    Add a simple pre filter to gateway

---
+++ gateway/src/main/java/net/blurblah/gateway/filters/SimplePreFilter.java
@@ -0,0 +1,40 @@
+package net.blurblah.gateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class SimplePreFilter extends ZuulFilter {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest req = ctx.getRequest();
+        log.info(String.format("%s reuqest to %s", req.getMethod(),
+                req.getRequestURL().toString()));
+        return null;
+    }
+}
